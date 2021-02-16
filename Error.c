ecb0f083c49fea0a92a92a1df7bb2870c0bd71ea
berezovs <arthas2103@LAPTOP-P78N46FO.localdomain>
2021-02-15 20:07:00 -0600

    Minor modifications in Error.c

--- Error.c
+++ Error.c
@@ -17,22 +17,22 @@ void printError()
     switch (errorCode)
     {
     case MISSING_BEGIN:
-        printf("Syntax error, expected 'begin' on line: %d\n", lNumber);
+        printf("Syntax error: expected 'begin' on line %d\n", lNumber);
         break;
     case MISSING_END:
-        printf("Syntax error,expected 'end' on line: %d\n", lNumber);
+        printf("Syntax error: expected 'end' on line %d\n", lNumber);
         break;
     case MISSING_ASSIGN_OPERATOR:
-        printf("Syntax error, expected '=' on line: %d\n", lNumber);
+        printf("Syntax error: expected '=' on line %d\n", lNumber);
         break;
     case ILLEGAL_IDENTIFIER:
-        printf("Syntax error, illegal identifier on line: %d\n", lNumber);
+        printf("Syntax error: illegal identifier on line %d\n", lNumber);
         break;
     case MISSING_CLOSING_PAR:
-        printf("Syntax error, expected ')' on line: %d\n", lNumber);
+        printf("Syntax error: expected ')' on line %d\n", lNumber);
         break;
     case MISSING_PERIOD:
-        printf("Syntax error, expected '.' on line: %d\n", lNumber);
+        printf("Syntax error: expected '.' on line %d\n", lNumber);
         break;
     default:
         printf("Syntax error: unexpected token on line %d\n", lNumber);
